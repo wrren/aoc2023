74e0e1b359286df72f898bb2ce6297a100edefe4
Warren Kenny <wkenny@riotgames.com>
2023-12-04 13:23:57 +0000

    Added solution for day 4

--- cpp/Common/aoc/input.h
+++ cpp/Common/aoc/input.h
@@ -29,4 +29,57 @@ namespace aoc
 
         return true;
 	}
+
+    std::vector<std::string> split(const std::string& input, const std::string& delimiter)
+    {
+        std::vector<std::string> split;
+
+        auto start = input.find_first_not_of(delimiter);
+
+        while (start != std::string::npos)
+        {
+            auto end = input.find_first_of(delimiter, start + 1);
+
+            if (end != std::string::npos)
+            {
+                split.push_back(input.substr(start, end - start));
+            }
+            else
+            {
+                split.push_back(input.substr(start));
+                break;
+            }
+
+            start = input.find_first_not_of(delimiter, end + 1);
+        }
+
+        return split;
+    }
+
+    template<typename T>
+    std::vector<T> split_transform(const std::string& input, const std::string& delimiter, std::function<T(const std::string&)> transformer)
+    {
+        std::vector<T> split;
+
+        auto start = input.find_first_not_of(delimiter);
+
+        while (start != std::string::npos)
+        {
+            auto end = input.find_first_of(delimiter, start + 1);
+
+            if (end != std::string::npos)
+            {
+                split.push_back(transformer(input.substr(start, end - start)));
+            }
+            else
+            {
+                split.push_back(transformer(input.substr(start)));
+                break;
+            }
+
+            start = input.find_first_not_of(delimiter, end + 1);
+        }
+
+        return split;
+    }
 }
